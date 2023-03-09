a23ee66ca761e286048c67c93bef9bb483048f88
durbar chakraborty <durbardibyo@gmail.com>
2023-03-10 00:08:34 +0530

    updated imports and package json

--- weather_application/src/index.ts
+++ weather_application/src/index.ts
@@ -3,19 +3,90 @@ import {
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
-import { ICommandPalette } from '@jupyterlab/apputils';
+import { ICommandPalette, MainAreaWidget } from '@jupyterlab/apputils';
 
-/**
- * Initialization data for the weather_application extension.
- */
+import { Widget } from '@lumino/widgets';
+
+
 const plugin: JupyterFrontEndPlugin<void> = {
-  id: 'jupyterlab_weather_application',
+  id: 'jupyterlab-apod',
   autoStart: true,
   requires: [ICommandPalette],
+
   activate: (app: JupyterFrontEnd, palette: ICommandPalette) => {
-    console.log('JupyterLab extension weather_application is activated!');
-    console.log('ICommandPalette:', palette);
+
+    console.log('JupyterLab extension jupyterlab_apod is activated!');
+
+
+
+    // Define a widget creator function,
+
+    // then call it to make a new widget
+
+    const newWidget = () => {
+
+      // Create a blank content widget inside of a MainAreaWidget
+
+      const content = new Widget();
+
+      const widget = new MainAreaWidget({ content });
+
+      widget.id = 'apod-jupyterlab';
+
+      widget.title.label = 'Astronomy Picture';
+
+      widget.title.closable = true;
+
+      return widget;
+
+    }
+
+    let widget = newWidget();
+
+
+
+    // Add an application command
+
+    const command: string = 'apod:open';
+
+    app.commands.addCommand(command, {
+
+      label: 'Random Astronomy Picture',
+
+      execute: () => {
+
+        // Regenerate the widget if disposed
+
+        if (widget.isDisposed) {
+
+          widget = newWidget();
+
+        }
+
+        if (!widget.isAttached) {
+
+          // Attach the widget to the main work area if it's not there
+
+          app.shell.add(widget, 'main');
+
+        }
+
+        // Activate the widget
+
+        app.shell.activateById(widget.id);
+
+      }
+
+    });
+
+
+
+    // Add the command to the palette.
+
+    palette.addItem({ command, category: 'Tutorial' });
+
   }
+
 };
 
 export default plugin;
